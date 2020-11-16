de52121af85cb89424f970c2906f32210a685117
Joppeloppe <joakimlevin@hotmail.com>
2020-11-16 12:15:46 +0100

    Build + Restart
    There is now a Desktop build, navigate into Builds folder and run Boids3D.exe
    
    Added a Restart function, there is now a button in the top right that will restart the simulation.

---
+++ Assets/Scripts/RestartScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartScene : MonoBehaviour
+{
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
